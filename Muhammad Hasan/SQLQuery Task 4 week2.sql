1dbfd2aae3a1558df691c92410271c04e1337534
Muhammad Hasan <65036956+MuhammadHasan786@users.noreply.github.com>
2023-03-22 15:28:30 +0500

    SQL Query Task4 week2
    Creating, Altering & Deleting Database.

---
+++ Muhammad Hasan/SQLQuery Task 4 week2.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE Mhasan;
+CREATE TABLE Persons
+(
+PersonID int,
+LastName varchar(255),
+FirstName varchar(255),
+Address varchar(255),
+City varchar(255)
+);
+
+select * from Persons;
+
+DROP DATABASE Mhasan;
