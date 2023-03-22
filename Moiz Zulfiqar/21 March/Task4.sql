335a87fc01c5bdcf534128ec3d72373ce1f22b9b
abdulrazaque103 <abdulrazaquerajput9@gmail.com>
2023-03-22 23:00:20 +0500

    Task of first week

--- Moiz Zulfiqar/21 March/Task4.sql
+++
@@ -1,4 +0,0 @@
--- CREATE DATABASE ExampleDB
--- CREATE DATABASE NewDB
--- Alter database ExampleDB Modify Name = TaskDB
-DROP DATABASE NewDB
--- Muhammad_Fahad/README.md
+++
@@ -1,2 +0,0 @@
-# BWT_Data_Engineering_Repository
-Bytewise Data Engineernig Fellowship - M. Fahad
