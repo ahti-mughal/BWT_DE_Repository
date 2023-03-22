6a6ff984428e2d464459b34ecfa03ab1b3d33afc
Muhammad Waqar Gul <waqargul6@gmail.com>
2023-03-22 11:01:28 +0500

    Added 21st March 2023 Tasks
    Tasks contain .docx and .sql files

---
+++ Muhammad Waqar Gul/SQLQuery1 - 21 March 2023.sql
@@ -0,0 +1,8 @@
+-- create a new database
+Create database MyFirstDB;
+
+-- alter database name
+alter database MyFirstDB modify name = RenamedDB;
+
+-- delete/drop a database
+drop database RenamedDB;
