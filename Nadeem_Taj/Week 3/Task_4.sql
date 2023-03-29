d9592b866ce4fa9b28e13adb88821ee2e9eabfea
Nadeem Taj <2018ee335@student.uet.edu.pk>
2023-03-29 16:52:21 +0500

    Task3 completed

---
+++ Nadeem_Taj/Week 3/Task_4.sql
@@ -0,0 +1,14 @@
+use Northwind
+-- 1 - Write a query to show a list of customers and their corresponding orders.
+select*from Customers 
+
+--2 - Write a query to show a list of employees and their corresponding managers:
+
+
+
+--3 - Write a query to show a list of customers who have not placed any orders.
+
+
+
+
+--4 - Write a query to show a list of employees and their corresponding territories, including those employees who have no territories assigned.
