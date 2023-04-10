5da1056d1562b362053c969f6897283de2a1a3cd
ZapeeoSheikh <muhammadrmz871@gmail.com>
2023-04-11 00:20:28 +0500

    string, types, slice

---
+++ Muhammad_Rameez/Week_04/pratice01.py
@@ -0,0 +1,59 @@
+# =================== Comments =================================
+# Comments ( When we want to save some information related to code
+# but we dont want to run it in the code then we use comment )
+# Use hash # symbol in the beginning of the code to comment that line in the code
+
+# =================== Print ====================================
+# print("hello world")
+# print("My name is Muhammad Rameez")
+# print("I am 21 years old")
+
+# =================== Data Types ====================================
+# name = "Muhammad Rameez"
+# age = 21
+# is_graduated = False
+# height = 5.9
+
+# print("My name is " + name)
+# print(f"I am {age} years old")
+# print(f"My height is {height}")
+# print(f"Did you feel I am graduated ?? {is_graduated}")
+
+# we don't need to declare datatype in python
+
+# =================== Types =============================
+# ran = 1j
+# ran1 = 1
+# ran2 = 1.23
+# ran3 = 'Muhammad Rameez'
+# print(type(ran))
+# print(type(ran1))
+# print(type(ran2))
+# print(type(ran3))
+
+# =================== Casting =============================
+# name = 'My Name'
+# age = float(21)
+# height = str(9)
+
+# print('My name is ' + str(name) + " My age is " +
+#       str(age) + ' My height is ' + height)
+
+
+# =================== String Slicing =============================
+# name = 'Muhammad Rameez'
+# print(name[0])
+# print(name[4])
+# print(name[0:5])
+# print(name[3:5])
+# print(name[-2])
+
+# =================== String functions =============================
+import math
+name = "Muhammad Rameez"
+
+# print(name.capitalize())
+# print(name.lower())
+print(name.upper())
+# print(name.index(5))
+# print(name.split(' ')[0])
